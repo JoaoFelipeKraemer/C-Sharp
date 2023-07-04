4e57e11745f04436776ca0c9df5d6985b8d92ea1
João Felipe Kraemer Silveira <jo_oaoo@hotmail.com>
2023-07-04 15:41:02 -0300

    dia2

---
+++ dia2/exemploCast/exemplocast/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        // mostrar valor
+        // pedir para digitar numero de parcelas
+        // informar valor da parcela
+        double totalValue = 152.60;
+        Console.WriteLine("valor total é " + totalValue);
+        string? parcelas = Console.ReadLine();
+        int convertParcelas = Convert.ToInt32(parcelas);
+        double amount = totalValue / convertParcelas;
+        Console.WriteLine("O valor da parcela é: " + amount);
+    }
+}
