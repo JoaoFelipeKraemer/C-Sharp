a08994e974369b3db1d413c3981346689cfea390
João Felipe Kraemer Silveira <jo_oaoo@hotmail.com>
2023-07-12 16:31:00 -0300

    classes e instancias

---
+++ sec2/dia1/movie/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        var movie = new Movie("Matrix", "Ficçao");
+        var movie2 = new Movie("LOTR", "Fantasia");
+        Console.WriteLine(movie.Title);
+        Console.WriteLine(movie2.Title);
+        movie.Title = "Matrix2";
+    }
+}
