6f4fbaa76598ddb175f95f28986472f1e20434d2
João Felipe Kraemer Silveira <jo_oaoo@hotmail.com>
2023-07-13 17:26:19 -0300

    poli

---
+++ sec2/dia3/poli/animal-kingdom/Wolf.cs
@@ -0,0 +1,8 @@
+public class Wolf : Animal
+{
+    public override string ScientificName => "Canis lupus";
+    public override bool CanEatMeat => true;
+    public override bool CanEatVegetables => false;
+
+
+}
